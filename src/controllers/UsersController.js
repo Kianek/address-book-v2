798d501804e13e57c6b98075081d793930249b23
Keanu Allridge <kallridge92@gmail.com>
2019-10-20 16:36:29 -0700

    Initial commit

---
+++ src/controllers/UsersController.js
@@ -0,0 +1,8 @@
+module.exports = function UsersController(UserModel) {
+  this.createUser = async function(req, res) {};
+  this.updateUser = async function(req, res) {};
+  this.deleteUser = async function(req, res) {};
+
+  this.logIn = async function(req, res) {};
+  this.logOut = async function(req, res) {};
+};
