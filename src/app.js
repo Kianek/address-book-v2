adf6faf91382d0a2964cc172a25475850c2508f0
Keanu Allridge <kallridge92@gmail.com>
2019-10-20 16:36:16 -0700

    Import routes

--- src/app.js
+++ src/app.js
@@ -15,6 +15,14 @@ db.authenticate()
   .then(() => console.log('Connected to DB'))
   .catch(err => console.error(err));
 
+// Import routes
+const users = require('./routes/users');
+const contacts = require('./routes/contacts');
+
+app.use('/users', users);
+app.use('/contacts', contacts);
+
+// Start server
 const port = process.env.PORT;
 app.listen(port, () =>
   console.log(`Server running on http://localhost:${port}`)
