0186a2063450b0170c3b63248df0e114eab3d498
Keanu Allridge <kallridge92@gmail.com>
2019-10-20 17:59:21 -0700

    Add methods

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,8 +1,40 @@
-module.exports = function UsersController(UserModel) {
-  this.createUser = async function(req, res) {};
-  this.updateUser = async function(req, res) {};
-  this.deleteUser = async function(req, res) {};
+const UserService = require('../services/UserService');
+const { plainify } = require('../helpers/utils');
 
-  this.logIn = async function(req, res) {};
-  this.logOut = async function(req, res) {};
+module.exports = function UsersController(UserModel) {
+  this.createUser = async function(req, res) {
+    try {
+      const service = new UserService(UserModel);
+
+      const result = await service.add(req.body);
+      if (!user) return res.status(500).json({ msg: 'Unable to add user' });
+
+      return res.status(201).json(plainify(result));
+    } catch (err) {
+      return res.status(500).json({ msg: 'Unable to add user' });
+    }
+  };
+
+  this.updateUser = async function(req, res) {
+    try {
+      const service = new UserService(UserModel);
+      const result = await service.update(req.params.id, req.body);
+    } catch (err) {
+      return status(500).json({ msg: 'Unable to update user' });
+    }
+  };
+
+  this.deleteUser = async function(req, res) {
+    try {
+      const service = new UserService(UserModel);
+      const result = await service.remove(req.params.id);
+
+      if (!result)
+        return res.status(500).json({ msg: 'Unable to delete user' });
+
+      return res.status(204).json(result);
+    } catch (err) {
+      return status(500).json({ msg: 'Unable to update user' });
+    }
+  };
 };
