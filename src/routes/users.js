496044cb91e2e49d05dcb04809fa8da1248a81e9
Keanu Allridge <kallridge92@gmail.com>
2019-10-20 17:59:35 -0700

    Add routes

---
+++ src/routes/users.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+const { User } = require('../config/database').User;
+const UserController = require('../controllers/UsersController');
+const controller = new UserController(User);
+
+router.get('/test', (req, res) => res.send('Users works'));
+
+router.post('/', controller.createUser);
+router.put('/:id', controller.updateUser);
+router.delete('/:id', controller.deleteUser);
+
+module.exports = router;
