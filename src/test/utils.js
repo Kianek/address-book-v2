7e041973f6152acafd83a804341a9202899b3d6e
Keanu Allridge <kallridge92@gmail.com>
2019-10-21 15:43:49 -0700

    Add seedContacts function

--- src/test/utils.js
+++ src/test/utils.js
@@ -29,8 +29,32 @@ async function seedUsers(numOfUsers) {
   return Promise.all(users);
 }
 
+async function seedContacts(userId, numOfContacts) {
+  const contacts = [];
+  for (let i = 0; i < numOfContacts; i++) {
+    contacts.push(
+      Contact.create({
+        userId,
+        firstName: faker.fake('{{name.firstName}}'),
+        middleName: faker.fake('{{name.firstName}}'),
+        lastName: faker.fake('{{name.lastName}}'),
+        phone: faker.phone.phoneNumber(),
+        email: faker.internet.email(),
+        line1: faker.address.streetAddress(),
+        line2: null,
+        city: faker.address.city(),
+        stateOrProvince: faker.address.state(),
+        postalCode: faker.address.zipCode(),
+      })
+    );
+  }
+
+  return Promise.all(contacts);
+}
+
 module.exports = {
   seedUsers,
+  seedContacts,
   initDatabase,
   clearDatabase,
   closeDatabase,
