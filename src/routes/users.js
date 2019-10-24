af6cef72d9ad3ae6b43d49ad202eda6b5bfe5536
Keanu Allridge <kallridge92@gmail.com>
2019-10-24 06:52:54 -0700

    Add authentication

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,12 +1,15 @@
 const router = require('express').Router();
 const { User } = require('../config/database');
+const { ensureAuthenticated } = require('../helpers/utils');
 const UserController = require('../controllers/UsersController');
 const controller = new UserController(User);
 
 router.get('/test', (req, res) => res.send('Users works'));
 
 router.post('/', controller.createUser);
-router.put('/:id', controller.updateUser);
-router.delete('/:id', controller.deleteUser);
+
+router.put('/:id', ensureAuthenticated, controller.updateUser);
+
+router.delete('/:id', ensureAuthenticated, controller.deleteUser);
 
 module.exports = router;
