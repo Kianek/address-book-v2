7413c417f48fa6c5d69c01a506769234ac03a4fb
Keanu Allridge <kallridge92@gmail.com>
2019-10-24 06:53:07 -0700

    Fill in routes

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,8 +1,12 @@
 const router = require('express').Router();
+const passport = require('passport');
+const AuthController = require('../controllers/AuthController');
+const controller = new AuthController();
 
 router.get('/test', (req, res) => res.send('Auth works'));
 
-router.post('/login', (req, res) => {});
-router.get('/logout', (req, res) => {});
+router.post('/login', passport.authenticate('local'), controller.login);
+
+router.get('/logout', controller.logout);
 
 module.exports = router;
