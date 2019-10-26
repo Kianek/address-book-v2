b6b45f36b7351fa05cf9cef2141b54547b748a48
Keanu Allridge <kallridge92@gmail.com>
2019-10-25 21:28:43 -0700

    Add tests

--- src/test/controllers/ContactsController.test.js
+++ src/test/controllers/ContactsController.test.js
@@ -17,10 +17,56 @@ after(async () => {
   await clearDatabase();
 });
 
-describe('ContactsController', function() {
+describe.only('ContactsController', function() {
   it('should create a new contact', async function() {
     try {
       const [user] = await seedUsers(1);
+
+      const agent = request.agent(app);
+      await agent
+        .post('/auth/login')
+        .type('application/json')
+        .send({ email: user.email, password: 'password123' });
+
+      const newContact = {
+        userId: user.id,
+        firstName: faker.name.firstName(),
+        middleName: faker.name.firstName(),
+        lastName: faker.name.lastName(),
+        phone: faker.phone.phoneNumber(),
+        email: faker.internet.email(),
+        line1: faker.address.streetAddress(),
+        line2: null,
+        city: faker.address.city(),
+        stateOrProvince: faker.address.state(),
+        postalCode: faker.address.zipCode(),
+      };
+
+      const result = await agent
+        .post('/contacts')
+        .type('application/json')
+        .send(newContact);
+
+      expect(result.body.firstName).to.equal(newContact.firstName);
+    } catch (err) {
+      console.error(err);
+    }
+  });
+
+  it('should load all saved contacts', async function() {
+    try {
+      const [user] = await seedUsers(1);
+      await seedContacts(user.id, 4);
+
+      const agent = request.agent(app);
+      await agent
+        .post('/auth/login')
+        .type('application/json')
+        .send({ email: user.email, password: 'password123' });
+
+      const result = await agent.get('/contacts');
+
+      expect(result.body.length).to.equal(4);
     } catch (err) {
       console.error(err);
     }
