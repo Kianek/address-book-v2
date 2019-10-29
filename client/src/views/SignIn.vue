d183502d7dc9f50e02fb23957b927ac2d896343f
Keanu Allridge <kallridge92@gmail.com>
2019-10-29 06:57:19 -0700

    Tweaks; add loading property

--- client/src/views/SignIn.vue
+++ client/src/views/SignIn.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="sign-in">
+  <Page>
     <h1>Sign In</h1>
     <Form :submit="submit">
       <Input v-model="email" placeholder="Email" />
@@ -7,31 +7,39 @@
       <Input type="submit" value="Sign In" />
     </Form>
     <router-link class="link" to="/register">No account? Register here.</router-link>
-  </div>
+  </Page>
 </template>
 
 <script>
+import Page from "@/components/layout/Page.vue";
 import Form from "@/components/shared/Form.vue";
 import Input from "@/components/shared/Input.vue";
+import Spinner from "@/components/shared/Spinner.vue";
 
 export default {
   data() {
     return {
       email: "",
-      password: ""
+      password: "",
+      isLoading: false
     };
   },
   methods: {
     submit: function(e) {
       e.preventDefault();
+      this.isLoading = !this.isLoading;
       this.email = "";
       this.password = "";
+
       console.log("Signing in!");
+      setTimeout(() => this.$router.push("/dashboard"), 3000);
     }
   },
   components: {
+    Page,
     Form,
-    Input
+    Input,
+    Spinner
   }
 };
 </script>
