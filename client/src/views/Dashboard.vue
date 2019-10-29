58f9b5b265e5fd52acb7e7138aca2db5c34afc68
Keanu Allridge <kallridge92@gmail.com>
2019-10-29 06:57:40 -0700

    Fill in markup and styles

--- client/src/views/Dashboard.vue
+++ client/src/views/Dashboard.vue
@@ -1,12 +1,81 @@
 <template>
-  <div class="dashboard">
-    <h1>Dashboard</h1>
-  </div>
+  <Page>
+    <h1>Contacts</h1>
+    <router-link to="#" class="add">
+      <i class="fas fa-user-plus"></i> Add
+    </router-link>
+    <ul class="contacts-list">
+      <li v-for="contact in contacts" :key="contact.id">
+        <ContactPreview :contact="contact" />
+      </li>
+    </ul>
+  </Page>
 </template>
 
 <script>
+import Page from "@/components/layout/Page";
+// import List from "@/components/shared/List";
+import ContactPreview from "@/components/ContactPreview";
+
 export default {
   name: "dashboard",
-  components: {}
+  data() {
+    return {
+      contacts: [
+        {
+          id: 1,
+          firstName: "Bob",
+          middleName: "B.",
+          lastName: "Loblaw",
+          phone: "444-444-4444",
+          email: "bob@loblawlaw.com",
+          line1: "123 Street St",
+          line2: null,
+          city: "Soylandia",
+          stateOrProvince: "Pooregon",
+          postalCode: "97205"
+        }
+      ]
+    };
+  },
+  components: {
+    Page,
+    // List,
+    ContactPreview
+  }
 };
 </script>
+
+<style lang="scss" scoped>
+.add {
+  background-color: #fff;
+  border-radius: 5px;
+  // border: solid 1px lightgray;
+  // color: #00d300;
+  color: #000;
+  padding: 0.5em;
+  text-decoration: none;
+  transition: 0.2s;
+
+  &:hover {
+    background-color: #00d300;
+    color: #fff;
+  }
+}
+
+.contacts-list {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 1rem;
+  padding: 0;
+  list-style-type: none;
+  width: 100%;
+
+  li {
+    width: 50%;
+    min-width: 200px;
+    max-width: 300px;
+  }
+}
+</style>
