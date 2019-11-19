f414786458d38f3679fa67079512d8c72cec35f9
Keanu Allridge <keanuallridge@keanus-mbp.cwllp.local>
2019-11-19 13:46:15 -0800

    Fix formatting

--- client/src/App.vue
+++ client/src/App.vue
@@ -25,9 +25,12 @@ export default {
   box-sizing: border-box;
 }
 #app {
+  display: flex;
+  flex-direction: column;
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
+  min-height: 100vh;
 }
 </style>
