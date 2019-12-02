aa8bdaf0c8ac06c7473c2621f64d8a96d080db11
Keanu Allridge <keanuallridge@Keanus-MBP.domain>
2019-12-02 06:31:25 -0800

    Add additional linting config

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -3,11 +3,21 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: ['plugin:vue/essential', '@vue/prettier'],
+  extends: ['plugin:vue/essential', 'eslint:recommended'],
   rules: {
+    'vue/component-name-in-template-casing': ['error', 'PascalCase'],
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    quotes: ['error', 'double'],
+    'vue/max-attributes-per-line': [
+      'error',
+      {
+        singleline: 1,
+        multiline: {
+          max: 1,
+          allowFirstLine: false,
+        },
+      },
+    ],
   },
   parserOptions: {
     parser: 'babel-eslint',
