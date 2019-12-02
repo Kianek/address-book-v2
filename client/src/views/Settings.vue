5601c095d9bb409aa707b8a9fff003e77cbfb488
Keanu Allridge <keanuallridge@Keanus-MBP.domain>
2019-12-02 06:33:00 -0800

    Add some boilerplate

--- client/src/views/Settings.vue
+++ client/src/views/Settings.vue
@@ -1,5 +1,18 @@
 <template>
-  <div>
+  <Page>
+    <FormBackButton url="/dashboard" />
     <h1>Settings</h1>
-  </div>
+  </Page>
 </template>
+
+<script>
+import Page from "../components/layout/Page.vue";
+import FormBackButton from "../components/shared/FormBackButton.vue";
+
+export default {
+  components: {
+    Page,
+    FormBackButton
+  }
+};
+</script>
