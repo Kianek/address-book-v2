15a81a81aa2798f13bd0133d5d79c96109d91b70
Keanu Allridge <keanuallridge@Keanus-MBP.domain>
2019-12-02 06:31:48 -0800

    Initial commit

---
+++ client/src/components/shared/FormBackButton.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="back-to-dashboard">
+    <router-link
+      class="btn"
+      :to="url"
+    >
+      <i class="fas fa-arrow-left" /> Back
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    url: String
+  }
+};
+</script>
+
+<style>
+.back-to-dashboard {
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  margin-top: 1rem;
+  margin-bottom: 1rem;
+  width: 100%;
+}
+
+.btn {
+  background-color: #0f6fff;
+  border-radius: 5px;
+  color: white;
+  padding: 0.5em 0.7em;
+  text-decoration: none;
+}
+</style>
