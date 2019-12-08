d8b22d96c8a096ecaebf60732f06d4e7e88296e7
Keanu Allridge <keanuallridge@Keanus-MBP.domain>
2019-12-07 18:47:34 -0800

    Initial commit

---
+++ client/src/views/ChangeEmail.vue
@@ -0,0 +1,20 @@
+<template>
+  <Page>
+    <BackButton url="/settings" />
+  </Page>
+</template>
+
+<script>
+import Page from "@/components/layout/Page.vue";
+import BackButton from "@/components/shared/BackButton.vue";
+
+export default {
+  components: {
+    BackButton,
+    Page
+  }
+};
+</script>
+
+<style>
+</style>
