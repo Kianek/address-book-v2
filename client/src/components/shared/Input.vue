42dae22a682b1748ee4852bd1a817981ac5e11a8
Keanu Allridge <keanuallridge@Keanus-MBP.domain>
2019-12-08 10:27:28 -0800

    Add input validation

--- client/src/components/shared/Input.vue
+++ client/src/components/shared/Input.vue
@@ -1,31 +1,101 @@
 <template>
-  <input
-    :type="type"
-    :value="value"
-    :class="classes ? classes : ''"
-    :placeholder="required ? `${placeholder}*` : placeholder"
-    @change="$emit('change', $event.target.value)"
-  />
+  <div>
+    <p id="errorMsg">{{ error }}</p>
+    <input
+      :type="type"
+      :value="value"
+      :class="classObj"
+      :placeholder="required ? `${placeholder}*` : placeholder"
+      @keyup="$emit('keyup', $event.target.value)"
+    />
+  </div>
 </template>
 
 <script>
+import { isEmail } from "validator";
+
 export default {
+  data() {
+    return {
+      classObj: {
+        error: false
+      },
+      error: ""
+    };
+  },
   model: {
     prop: "value",
-    event: "change"
+    event: "keyup"
   },
   props: {
-    classes: String,
-    placeholder: String,
-    type: {
-      type: String,
-      default: "text"
+    max: {
+      type: Number,
+      default: 40
     },
+    min: {
+      type: Number,
+      default: 1
+    },
+    email: {
+      type: Boolean,
+      default: false
+    },
+    placeholder: String,
     required: {
       type: Boolean,
       default: false
     },
+    type: {
+      type: String,
+      default: "text"
+    },
     value: String
+  },
+  methods: {
+    isInvalidEmail(val) {
+      if (!isEmail(val)) {
+        this.error = "Must be a valid email";
+        return true;
+      }
+
+      return false;
+    },
+    inputIsTooShort(val) {
+      if (val <= this.min) {
+        this.error = `Must be at least ${this.min} character${
+          this.min > 1 ? "s" : null
+        }`;
+        return true;
+      }
+
+      return false;
+    },
+    inputIsTooLong(val) {
+      if (val > this.max) {
+        this.error = "Too long";
+        return true;
+      }
+
+      return false;
+    }
+  },
+  watch: {
+    value: function(val) {
+      const errors = [this.inputIsTooShort, this.inputIsTooLong];
+
+      // If input is an email field, add email validation
+      if (this.email) {
+        errors.push(this.isInvalidEmail);
+      }
+
+      // Check whether value is too short or long
+      this.classObj.error = errors.some(func => func(val));
+
+      // If no input error, remove any error message
+      if (!this.classObj.error) {
+        this.error = "";
+      }
+    }
   }
 };
 </script>
@@ -34,6 +104,11 @@ export default {
 @import "@/_colors.scss";
 @import "@/_mixins.scss";
 
+#errorMsg {
+  color: $red;
+  font-size: 0.5rem;
+}
+
 input {
   border: solid 1px $light-gray;
   border-radius: 5px;
@@ -41,6 +116,7 @@ input {
   margin: 0 auto 1em;
   padding: 0.5em;
   min-width: 200px;
+  transition: 0.2s;
   width: 80%;
 
   &:focus {
