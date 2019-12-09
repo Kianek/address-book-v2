d1d4ad100777786794d51d096758301aa34685d5
Keanu Allridge <keanuallridge@Keanus-MBP.domain>
2019-12-08 16:22:12 -0800

    Fill in email form

--- client/src/views/ChangeEmail.vue
+++ client/src/views/ChangeEmail.vue
@@ -1,17 +1,45 @@
 <template>
   <Page>
     <BackButton url="/settings" />
+    <h1>Change Email</h1>
+    <Form :submit="handleSubmit">
+      <Input
+        :min="5"
+        email
+        required
+        placeholder="New Email"
+        v-model="email"
+      />
+      <SubmitButton value="Update" />
+    </Form>
   </Page>
 </template>
 
 <script>
 import Page from "@/components/layout/Page.vue";
 import BackButton from "@/components/shared/BackButton.vue";
+import Form from "@/components/shared/Form.vue";
+import Input from "@/components/shared/Input.vue";
+import SubmitButton from "@/components/shared/SubmitButton.vue";
 
 export default {
+  data() {
+    return {
+      email: ""
+    };
+  },
+  methods: {
+    handleSubmit() {
+      console.log("submitting");
+      this.$router.push("/settings");
+    }
+  },
   components: {
     BackButton,
-    Page
+    Page,
+    Form,
+    Input,
+    SubmitButton
   }
 };
 </script>
