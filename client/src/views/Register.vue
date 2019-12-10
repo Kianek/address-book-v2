6337a3a8e75287a3751f8b3ebf4d050d1056c2e7
Keanu Allridge <keanuallridge@Keanus-MacBook-Pro.local>
2019-12-10 11:39:41 -0800

    Hook up component to API

--- client/src/views/Register.vue
+++ client/src/views/Register.vue
@@ -5,27 +5,43 @@
     <Form :submit="createAccount">
       <Input
         v-model="firstName"
+        :min="2"
         placeholder="First Name"
       />
       <Input
+        :min="2"
         v-model="lastName"
         placeholder="Last Name"
       />
       <Input
+        :min="5"
         v-model="email"
         placeholder="Email"
       />
+      <p
+        v-show="error.status"
+        class="error"
+      >{{ error.message }}</p>
       <Input
+        :min="6"
         type="password"
+        placeholder="New Password"
         v-model="password"
-        placeholder="Password"
       />
       <Input
+        :min="6"
         type="password"
-        v-model="confirmPassword"
         placeholder="Confirm Password"
+        v-model="confirmPassword"
       />
-      <SubmitButton value="Create Account" />
+      <SubmitButton
+        :disabled="disableBtn"
+        value="Create Account"
+      />
+      <p
+        v-if="registration.status !== undefined"
+        :class="registration.status ? 'success' : 'failure'"
+      >{{ registration.message }}</p>
     </Form>
   </Page>
 </template>
@@ -37,6 +53,8 @@ import Input from "@/components/shared/Input.vue";
 import BackButton from "@/components/shared/BackButton.vue";
 import SubmitButton from "@/components/shared/SubmitButton.vue";
 
+import { isEmpty } from "validator";
+
 export default {
   data() {
     return {
@@ -44,18 +62,65 @@ export default {
       lastName: "",
       email: "",
       password: "",
-      confirmPassword: ""
+      confirmPassword: "",
+      error: {
+        status: false,
+        message: "Passwords must match"
+      },
+      registration: {
+        message: "",
+        status: undefined
+      },
+      disableBtn: false
     };
   },
   methods: {
     createAccount: function() {
       console.log("creating account");
-      this.resetFields();
-      this.$router.push("/");
-    },
-    resetFields: function() {
-      const keys = Object.keys(this.$data);
-      keys.forEach(key => (this.$data[key] = ""));
+      if (this.password !== this.confirmPassword) {
+        this.error.status = true;
+        return;
+      }
+
+      const user = {
+        firstName: this.firstName,
+        lastName: this.lastName,
+        email: this.email,
+        password: this.password
+      };
+
+      // Make sure no fields are empty
+      const anyFieldsAreEmpty = Object.values(user).some(val => isEmpty(val));
+      if (anyFieldsAreEmpty) return;
+
+      this.axios
+        .post("/users", user)
+        .then(res => {
+          if (res.status === 201) {
+            this.disableBtn = true;
+            this.registration.status = true;
+            this.registration.message = "Success! Please sign in.";
+
+            console.log(res);
+          } else {
+            this.registration.status = false;
+            this.registration.message = "Error creating account...";
+          }
+        })
+        .catch(err => {
+          console.error(err);
+          this.registration.status = false;
+          return;
+        });
+    }
+  },
+  watch: {
+    confirmPassword: function(val) {
+      if (val !== this.password) {
+        this.error.status = true;
+      } else {
+        this.error.status = false;
+      }
     }
   },
   components: {
@@ -72,7 +137,18 @@ export default {
 @import "@/_colors.scss";
 @import "@/_mixins.scss";
 
-input[type="submit"] :hover {
-  @include set-box-shadow($gray);
+.failure {
+  color: $red;
+  font-size: 1rem;
+}
+
+.success {
+  color: $green;
+  font-size: 1rem;
+}
+
+.error {
+  color: $red;
+  font-size: 1em;
 }
 </style>
