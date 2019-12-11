2c6d881ef8f867d8a2da6dfe0f67a6fb1add3d35
Keanu Allridge <keanuallridge@Keanus-MBP.domain>
2019-12-11 06:54:43 -0800

    Add actions, mutations, getter

--- client/src/store/modules/contacts.js
+++ client/src/store/modules/contacts.js
@@ -1,3 +1,5 @@
+import { ADD_CONTACT, UPDATE_CONTACT, DELETE_CONTACT } from "./types.js";
+
 const state = {
   contacts: [],
   selectedContact: {}
@@ -11,19 +13,38 @@ const getters = {
 };
 
 const mutations = {
-  createContact: (state, payload) => {
+  [ADD_CONTACT](state, payload) {
     state.contacts = state.contacts.unshift(payload.contact);
   },
-  updateContact: (state, payload) => {
+  [UPDATE_CONTACT](state, payload) {
     state.contacts.forEach(c => {
       if (c.id === payload.contact.id) {
         c = payload.contact;
       }
     });
+  },
+  [DELETE_CONTACT](state, payload) {
+    state = state.contacts.filter(c => c.id !== payload.id);
   }
 };
 
-const actions = {};
+const actions = {
+  addContact({ commit }, contact) {
+    this.axios.post("/contacts", contact)
+      .then(res => commit(ADD_CONTACT, { contact: res.data }))
+      .catch(err => console.error(err));
+  },
+  updateContact({ commit }, updatedContact) {
+    this.axios.put(`/contacts/${updatedContact.id}`, updatedContact)
+      .then(res => commit(UPDATE_CONTACT, { contact: res.data }))
+      .catch(err => console.error(err));
+  },
+  deleteContact({ commit }, id) {
+    this.axios.delete(`/contacts/${id}`, id)
+      .then(res => commit(DELETE_CONTACT, { id: res.data.id }))
+      .catch(err => console.error(err));
+  },
+};
 
 export default {
   state,
