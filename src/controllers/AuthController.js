a50148fdca97d246a77ddd333e69b0600b8f0baa
Keanu Allridge <keanuallridge@Keanus-MBP.domain>
2019-12-14 08:39:00 -0800

    Include user id in response obj

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -1,15 +1,15 @@
 module.exports = function AuthController() {
-  this.login = function(req, res) {
+  this.login = function (req, res) {
     // The user has been confirmed to exist, and a session established,
     // so return any add'l user info
     if (!req.user) return res.status(500).json({ msg: 'Error logging in ' });
 
-    const { firstName, lastName, email } = req.user;
+    const { id, firstName, lastName, email } = req.user;
 
-    return res.status(200).json({ firstName, lastName, email });
+    return res.status(200).json({ id, firstName, lastName, email });
   };
 
-  this.logout = function(req, res) {
+  this.logout = function (req, res) {
     req.logout();
 
     return res.sendStatus(200);
