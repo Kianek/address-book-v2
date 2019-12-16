b3614d8ca5a4e095c68ee391d60811c275b5cf95
Keanu Allridge <keanuallridge@Keanus-MBP.domain>
2019-12-16 06:29:00 -0800

    Add input labels

--- client/src/views/EditContact.vue
+++ client/src/views/EditContact.vue
@@ -2,46 +2,57 @@
   <Page>
     <BackButton url="/dashboard" />
     <h1>Edit Contact</h1>
+
     <Form :submit="handleSubmit">
       <Input
         required
+        label="First Name"
         v-model="firstName"
         placeholder="First Name"
       />
       <Input
+        label="Middle Name"
         v-model="middleName"
         placeholder="Middle Name"
       />
       <Input
+        label="Last Name"
         required
         v-model="lastName"
         placeholder="Last Name"
       />
       <Input
+        label="Phone"
         v-model="phone"
         placeholder="Phone"
       />
       <Input
+        label="Email"
         v-model="email"
         placeholder="Email"
       />
       <Input
+        label="Line 1"
         v-model="line1"
         placeholder="Line 1"
       />
       <Input
+        label="Line 2"
         v-model="line2"
         placeholder="Line 2"
       />
       <Input
+        label="City"
         v-model="city"
         placeholder="City"
       />
       <Input
+        label="State"
         v-model="stateOrProvince"
         placeholder="State"
       />
       <Input
+        label="Zip"
         v-model="postalCode"
         placeholder="Zip"
       />
@@ -56,6 +67,8 @@ import Form from "@/components/shared/Form.vue";
 import Input from "@/components/shared/Input.vue";
 import BackButton from "@/components/shared/BackButton.vue";
 import SubmitButton from "@/components/shared/SubmitButton.vue";
+
+import { mapActions, mapGetters } from "vuex";
 
 export default {
   data() {
@@ -76,7 +89,40 @@ export default {
     handleSubmit() {
       console.log("updating contact");
       setTimeout(() => this.$router.push("/dashboard"), 3000);
-    }
+    },
+    loadForm() {
+      const currentContact = this.getSelectedContact;
+
+      // Create an array of the data object's keys in order to access
+      // the corresponding value in the selected contact.
+      const keys = Object.keys(this.$data);
+
+      // Iterate through the keys, setting the data object's values to
+      // those of the current contact.
+      keys.forEach(key => (this.$data[key] = currentContact[key]));
+
+      console.log(this.$data);
+      this.$nextTick().catch(err => console.error(err));
+    },
+    ...mapActions(["clearSelectedContact", "selectContact"])
+  },
+  computed: {
+    ...mapGetters(["getSelectedContact"])
+  },
+  mounted() {
+    this.selectContact(parseInt(this.$route.params.id))
+      .then(() => {
+        this.loadForm();
+      })
+      .catch(err => console.error(err));
+  },
+  beforeRouteUpdate(to, from, next) {
+    this.selectContact(parseInt(this.$route.params.id))
+      .then(() => {
+        this.loadForm();
+        next();
+      })
+      .catch(err => console.error(err));
   },
   components: {
     Page,
