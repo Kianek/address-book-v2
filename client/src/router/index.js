ec00f2b5786607465547ed6122d401b9463ed569
Keanu Allridge <keanuallridge@Keanus-MacBook-Pro.local>
2019-12-17 11:53:57 -0800

    Add auth guards

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import SignIn from "../views/SignIn.vue";
 import Register from "../views/Register.vue";
+import store from '../store';
 
 Vue.use(VueRouter);
 
@@ -9,43 +10,51 @@ const routes = [
   {
     path: "/",
     name: "sign-in",
-    component: SignIn
+    component: SignIn,
+    meta: { requiresAuth: false }
   },
   {
     path: "/register",
     name: "register",
-    component: Register
+    component: Register,
+    meta: { requiresAuth: false }
   },
   {
     path: "/dashboard",
     name: "Dashboard",
     component: () => import("../views/Dashboard.vue"),
+    meta: { requiresAuth: true }
   },
   {
     path: "/add",
     name: "AddContact",
     component: () => import("../views/AddContact.vue"),
+    meta: { requiresAuth: true }
   },
   {
     path: "/:id/edit",
     name: "EditContact",
     props: { default: true },
     component: () => import("../views/EditContact.vue"),
+    meta: { requiresAuth: true }
   },
   {
     path: "/settings",
     name: "settings",
     component: () => import("../views/Settings.vue"),
+    meta: { requiresAuth: true }
   },
   {
     path: "/change-email",
     name: "ChangeEmail",
     component: () => import("../views/ChangeEmail.vue"),
+    meta: { requiresAuth: true }
   },
   {
     path: "/change-password",
     name: "ChangePassword",
     component: () => import("../views/ChangePassword.vue"),
+    meta: { requiresAuth: true }
   },
 ];
 
@@ -54,5 +63,17 @@ const router = new VueRouter({
   base: process.env.BASE_URL,
   routes
 });
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    if (!store.state.user.isAuthenticated) {
+      next({ name: "sign-in" })
+    } else {
+      next();
+    }
+  } else {
+    next();
+  }
+})
 
 export default router;
