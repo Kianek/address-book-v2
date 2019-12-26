f191ebd3015267f8d3980b8bbf577d588a8c29cd
Keanu Allridge <keanuallridge@Keanus-MBP.domain>
2019-12-25 19:56:17 -0800

    Configure for deployment

--- client/src/main.js
+++ client/src/main.js
@@ -5,7 +5,7 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
-axios.defaults.baseURL = "http://localhost:8080";
+axios.defaults.baseURL = process.env.NODE_ENV === "production" ? "https://suspicious-liskov-c37a60.netlify.com" : "http://localhost:8080";
 Vue.use(VueAxios, axios);
 
 new Vue({
